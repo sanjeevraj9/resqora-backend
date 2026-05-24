4816e9ab73d4f58ab43cb1c3f5a16e40ace0d2a5
sanjeevraj9 <sanjeevraj952545@email.com>
2026-05-24 12:50:26 +0530

    add email function

--- src/main/java/org/resqora/security/EmailService.java
+++ src/main/java/org/resqora/security/EmailService.java
@@ -1,28 +1,45 @@
 package org.resqora.security;
 
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class EmailService {
-    private final JavaMailSender mailSender;
-    public void sendVerificationEmail(String toEmail,String token){
-        String link="https://resqora-api.onrender.com/api/auth/verify?token="+token;
 
-        SimpleMailMessage message= new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("ResQora - Verify your Email");
-        message.setText(
-                "Hello! Welcome to ResQora \uD83D\uDE97\\n\\n"+
-                "Please verify your email by clicking the link below:\n\n"+
-                link +"\n\n+"+
-                        "This link is valid for 24 hours. \n\n"+
-                        "Team ResQora"
+    @Value("${sendgrid.api-key}")
+    private String apiKey;
+
+    @Value("${sendgrid.from-email}")
+    private String fromEmail;
+
+    public void sendVerificationEmail(String toEmail, String token) {
+        String link = "https://resqora-api.onrender.com/api/auth/verify?token=" + token;
+
+        Email from = new Email(fromEmail);
+        Email to = new Email(toEmail);
+        String subject = "Resqora - Verify Your Email 🚗";
+        Content content = new Content("text/plain",
+                "Hello! Welcome to Resqora 🚗\n\n" +
+                        "Please verify your email:\n\n" +
+                        link + "\n\n" +
+                        "Team Resqora"
         );
-        mailSender.send(message);
+
+        Mail mail = new Mail(from, subject, to, content);
+        SendGrid sg = new SendGrid(apiKey);
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            sg.api(request);
+        } catch (Exception e) {
+            throw new RuntimeException("Email send failed: " + e.getMessage());
+        }
     }
 }
