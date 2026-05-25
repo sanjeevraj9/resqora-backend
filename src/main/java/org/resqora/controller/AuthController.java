5868d3b2527fb283667c484b6f68148d4c191eb3
sanjeevraj9 <sanjeevraj952545@email.com>
2026-05-25 23:25:03 +0530

    add properties file

--- src/main/java/org/resqora/controller/AuthController.java
+++ src/main/java/org/resqora/controller/AuthController.java
@@ -1,6 +1,7 @@
 package org.resqora.controller;
 
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.resqora.dto.request.LoginRequest;
@@ -15,36 +16,38 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-private final AuthService authService;
+    private final AuthService authService;
 
-@PostMapping("/register/user")
+    @PostMapping("/register/user")
     public ResponseEntity<AuthResponse> registerUser(
             @Valid @RequestBody RegisterUserRequest request
-){
-    return ResponseEntity.ok(authService.registerUser(request));
-}
+    ){
+        return ResponseEntity.ok(authService.registerUser(request));
+    }
 
-@PostMapping("/register/mechanic")
+    @PostMapping("/register/mechanic")
     public ResponseEntity<AuthResponse> registerMechanic(
             @Valid @RequestBody RegisterMechanicRequest request
-){
-    return ResponseEntity.ok(authService.registerMechanic(request));
-}
+    ){
+        return ResponseEntity.ok(authService.registerMechanic(request));
+    }
 
-@PostMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(
-        @Valid @RequestBody LoginRequest request
-        ){
-    return ResponseEntity.ok(authService.login(request));
-}
+            @Valid @RequestBody LoginRequest request
+    ){
+        return ResponseEntity.ok(authService.login(request));
+    }
 
-@GetMapping("/verify")
-    public ResponseEntity<String> verifyEmail(
-            @RequestParam String token
-){
-    authService.verifyEmail(token);
-    return ResponseEntity.ok("Email verified Successfully! You can now login.");
-}
+    @GetMapping("/verify")
+    public void verifyEmail(
+            @RequestParam String token,
+            HttpServletResponse response
+    )throws Exception {
+        authService.verifyEmail(token);
+        response.sendRedirect("https://resqora-9jtt.vercel.app/email-verified");
+
+    }
 
 
 }
